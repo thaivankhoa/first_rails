0a9fbc71654964650a4aa21ad07b0db8557b5ed6
thaivankhoa <thaivankhoa2802@gmail.com>
2017-03-14 12:27:15 +0700

    modified db/migrate

--- db/migrate/20170313083223_add_description_to_articles.rb
+++ db/migrate/20170313083223_add_description_to_articles.rb
@@ -2,6 +2,6 @@ class AddDescriptionToArticles < ActiveRecord::Migration[5.0]
   def change
   	add_column :articles, :description, :text
   	add_column :articles, :created_at, :datetime
-  	add_column :articles, :updated_at, :datetimex`
+  	add_column :articles, :updated_at, :datetimex
   end
 end
