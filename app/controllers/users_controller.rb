62e6f910c2f6774d8a248db43339170f16aa03ff
thaivankhoa <thaivankhoa2802@gmail.com>
2017-03-16 17:38:42 +0700

    Add restrict

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,9 @@ class UsersController < ApplicationController
 
 	before_action :set_user, only: [:edit, :update, :show]
 
+	# before_action :require_user, except: [:index, :show] 
+	before_action :require_same_user, only: [:edit, :update]
+
 	def index
 		@users = User.paginate(page: params[:page], per_page: 5)
 	end
@@ -48,4 +51,11 @@ class UsersController < ApplicationController
 		def set_user
 			@user = User.find(params[:id])
 		end
+
+		def require_same_user
+			if current_user != @user
+			flash[:danger] = "You can only edit your own account"
+			redirect_to root_path	
+		end
+	end
 end
