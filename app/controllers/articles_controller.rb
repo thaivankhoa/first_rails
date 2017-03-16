8a730db84cd17511adee7d82e65c3fae2249f5e1
thaivankhoa <thaivankhoa2802@gmail.com>
2017-03-16 17:41:43 +0700

    Add current user in articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,7 +20,7 @@ class ArticlesController < ApplicationController
 	def create
 		#render plain: params[:article].inspect
 		@article = Article.new(article_params)
-		@article.user = User.find(session[:user_id])
+		@article.user = current_user
 		if @article.save 
 			flash[:success] = "Article was succesfully created"
 			redirect_to article_path(@article)
