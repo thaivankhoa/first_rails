1d4dc67cf888b99b2b97cd79c072628a5d78467e
thaivankhoa <thaivankhoa2802@gmail.com>
2017-03-16 15:49:28 +0700

    Restriction UI, login form, create destroy session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
 
+	before_action :set_user, only: [:edit, :update, :show]
+
 	def index
-		@users = User.all
+		@users = User.paginate(page: params[:page], per_page: 5)
 	end
 
 	def new
@@ -20,11 +22,11 @@ class UsersController < ApplicationController
 	end
 
 	def edit
-		@user = User.find(params[:id])
+		
 	end
 
 	def update
-		@user = User.find(params[:id])
+		
 		if @user.update(user_params)
 			flash[:success] = "Your account has successfully edited"
 			redirect_to articles_path
@@ -34,11 +36,16 @@ class UsersController < ApplicationController
 	end
 
 	def show
-		@user = User.find(params[:id])
+		
+		@user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
 	end
 
 	private
 		def user_params
 			params.require(:user).permit(:username, :email, :password)
+		end
+
+		def set_user
+			@user = User.find(params[:id])
 		end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,12 @@ Rails.application.routes.draw do
   post 'users', to: 'users#create'
 
   resources :users, except: [:new]
+
+  get 'login', to: 'sessions#new'
+
+  post 'login', to: 'sessions#create'
+
+  delete 'logout', to: 'sessions#destroy'
   #resources :user
 
 end
